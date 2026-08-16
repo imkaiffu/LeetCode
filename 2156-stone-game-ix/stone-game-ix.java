f3cc256f90c5e12d4ef253d2b4faf35180ce0be8
MD Kaif Ansari <mrkaifansari000786@gmail.com>
2026-08-16 20:02:00 +0530

    Time: 7 ms (29.05%) | Memory: 114.6 MB (94.59%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int stone : stones) {
+            int rem = stone % 3;
+            if (rem == 0) {
+                cnt0++;
+            } else if (rem == 1) {
+                cnt1++;
+            } else {
+                cnt2++;
+            }
+        }
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
+        }
+        return Math.abs(cnt1 - cnt2) > 2;
+    }
+}
